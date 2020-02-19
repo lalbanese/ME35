2a8d2fcf52fe03b7168b547a1683e93cef54bf4a
lalbanese <60711012+lalbanese@users.noreply.github.com>
2020-02-19 13:03:46 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -29,37 +29,57 @@ ev3.speaker.beep()
 # (who knows where in the serial transmission the line is when I do the read).
 # '''
 
-count = 0
 imumeans = [0, 0, 0, 0, 0, 0]
+threshold = [3, 3, 5]
+button = TouchSensor(Port.S1)
 
 import serial
 s=serial.Serial("/dev/ttyACM0",9600)
-
 while True:
-     data=s.read(s.inWaiting()).decode("utf-8")
-     #print('size = %d, buffer = %d' % (len(data),s.inWaiting()))
-     if len(data) != 0:
-        data = data.splitlines()
-        imu = data[0].split(',')
-        #print(data)
-        #print(imu)
+    if button.pressed() == True:
+        count = 0
 
-        if imu == ['']:
-            continue
+        while button.pressed() == True:
+            data=s.read(s.inWaiting()).decode("utf-8")
+            #print('size = %d, buffer = %d' % (len(data),s.inWaiting()))
+            if len(data) != 0:
+                data = data.splitlines()
+                imu = data[0].split(',')
+                #print(data)
+                #print(imu)
 
-        count = count+1
-        try:
-            for i in range(len(imu)):
-                #print(type(imu[i]))
-                #print(imu[i])
-                imu[i] = float(imu[i])
+                if imu == ['']:
+                    continue
 
-            #print(type(imu[0]))
-            for means in range(len(imumeans)):
-                imumeans[means] = (imumeans[means]+imu[means])/(count)
+                try:
+                    
+                    for i in range(len(imu)):
+                        #print(type(imu[i]))
+                        #print(imu[i])
+                        imu[i] = float(imu[i])
+                        print(imu)
 
-        except:
-            print("ERROR")
-            print(data)
-            print(imu)
-            print(type(imu[0]))
+                    #print(type(imu[0]))
+                    for means in range(len(imumeans)):
+                        imumeans[means] = (imumeans[means]+imu[means])
+                        #print(imumeans)
+                    count = count+1
+
+                except:
+                    print("ERROR")
+                    #print(data)
+                    print(imu)
+                    #print(type(imu[0]))
+
+        print('buttonwaspressed')
+
+        for means in range(len(imumeans)):
+            imumeans[means] = imumeans[means]/count
+        #imumeans[0] = imumeans[0]-0.3
+        #imumeans[1] = imumeans[1]-0.16
+        #imumeans[2] = imumeans[2]-10
+
+        print(imumeans)
+
+        #if imumeans[1] > threshold[1]:
+         #   ev3.speaker.beep()
