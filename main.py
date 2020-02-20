1dd114aff68afed9248d998e48f0e9f555d0e0dc
lalbanese <60711012+lalbanese@users.noreply.github.com>
2020-02-20 15:17:54 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -7,79 +7,76 @@ from pybricks.tools import wait, StopWatch
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
+import serial
+import time
+
 # Write your program here
 ev3 = EV3Brick()
 ev3.speaker.beep()
 
-# '''
-# This reads the USB serial line on the EV3 as fast as possible
-# and assumes that data is coming from the Arduino much faster
-# than we can read it - so it grabs all the data, splits it up into an array,
-# and then takes the second to last element of that array to get an
-# array of accelerations and angular accelerations.
-# The 6 elements are:
-#      x acceleration
-#      y acceleration
-#      z acceleration
-#      x gyro
-#      y gyro
-#      z gyro
-     
-# Note that I take the second to last line because the last line might be incomplete
-# (who knows where in the serial transmission the line is when I do the read).
-# '''
-
-imumeans = [0, 0, 0, 0, 0, 0]
-threshold = [3, 3, 5]
-button = TouchSensor(Port.S1)
-
-import serial
-s=serial.Serial("/dev/ttyACM0",9600)
-while True:
-    if button.pressed() == True:
-        count = 0
-
-        while button.pressed() == True:
-            data=s.read(s.inWaiting()).decode("utf-8")
-            #print('size = %d, buffer = %d' % (len(data),s.inWaiting()))
-            if len(data) != 0:
-                data = data.splitlines()
-                imu = data[0].split(',')
-                #print(data)
-                #print(imu)
-
-                if imu == ['']:
+# Returns an array with IMU data
+def collectIMU(s):
+    tmp = []
+    while True:
+        data=s.read(s.inWaiting()).decode("utf-8")
+        if len(data) != 0:
+            data = data.splitlines()
+            data = data[0].split(',')
+            if len(data) == 6:
+                try:
+                    for item in data:
+                        tmp.append(float(item))
+                    return tmp
+                except:
                     continue
 
-                try:
-                    
-                    for i in range(len(imu)):
-                        #print(type(imu[i]))
-                        #print(imu[i])
-                        imu[i] = float(imu[i])
-                        print(imu)
 
-                    #print(type(imu[0]))
-                    for means in range(len(imumeans)):
-                        imumeans[means] = (imumeans[means]+imu[means])
-                        #print(imumeans)
-                    count = count+1
 
-                except:
-                    print("ERROR")
-                    #print(data)
-                    print(imu)
-                    #print(type(imu[0]))
+def averageCol(data, col):
+    tmp = []
+    for item in data:
+        tmp.append(item[col])
+    return sum(tmp) / len(tmp)
 
-        print('buttonwaspressed')
+def playMusic(imumeans, threshold, note_time):
+    global curr_time
+    # First check positive rotation
+    # Roll
+    if time.time() - curr_time < 0.1:
+        return
+    curr_time = time.time()
 
-        for means in range(len(imumeans)):
-            imumeans[means] = imumeans[means]/count
-        #imumeans[0] = imumeans[0]-0.3
-        #imumeans[1] = imumeans[1]-0.16
-        #imumeans[2] = imumeans[2]-10
+    if ((abs(imumeans[3]) > threshold[0]) and (imumeans[3] > 0)):
+        ev3.speaker.beep(261, note_time)
+    # Pitch
+    elif ((abs(imumeans[4]) > threshold[1]) and (imumeans[4] > 0)):
+        ev3.speaker.beep(220, note_time)
+    # Yaw
+    elif ((abs(imumeans[5]) > threshold[2]) and (imumeans[5] > 0)):
+        ev3.speaker.beep(196, note_time)
+    # Then check negative rotation
+    # Roll
+    elif ((abs(imumeans[3]) > threshold[0]) and (imumeans[3] < 0)):
+        ev3.speaker.beep(330, note_time)
+    # Pitch
+    elif ((abs(imumeans[4]) > threshold[1]) and (imumeans[4] < 0)):
+        ev3.speaker.beep(147, note_time)
+    # Yaw
+    elif ((abs(imumeans[5]) > threshold[2]) and (imumeans[5] < 0)):
+        ev3.speaker.beep(294, note_time)
 
-        print(imumeans)
+imumeans = [0, 0, 0, 0, 0, 0]
+threshold = [200, 200, 200]
+imuinit = []
+s=serial.Serial("/dev/ttyACM0",9600)
+note_time = 300
+curr_time = time.time()
 
-        #if imumeans[1] > threshold[1]:
-         #   ev3.speaker.beep()
+
+while True:
+    imuarray = collectIMU(s)
+    print(imuarray)
+
+    playMusic(imuarray, threshold, note_time)
+
+    #print(imumeans)
